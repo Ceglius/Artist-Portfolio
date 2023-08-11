89cd465d2d586e6d5d6a8bc2fc7f39d0e8b34bb5
DESKTOP-936A7SQ\Tomas-PC <tomas.pateckis@gmail.com>
2023-08-11 15:41:10 +0100

    path fix

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,4 @@
-import * as flsFunctions from "./files/functions.js";
+import * as flsFunctions from "./functions.js";
 flsFunctions.menuInit()
 flsFunctions.hideHeader()
 flsFunctions.placeholder()
@@ -90,18 +90,24 @@ function blogsSearch() {
   const blogForm = document.querySelector(".form-blog");
   const blogFormInput = document.querySelector(".form-blog__input");
   const blogsTitles = document.querySelectorAll(".card-blog__body h2");
-
+  const cardHide = document.querySelectorAll(".card-blog._hide");
   if (blogForm) {
     blogForm.addEventListener("keyup", () => {
       const inputValue = blogFormInput.value.toLowerCase();
-
-      blogsTitles.forEach((title) => {
+      
+      blogsTitles.forEach((title,) => {
+        
         const card = title.closest(".card-blog");
         if (title.textContent.toLowerCase().includes(inputValue)) {
-          card.style.display = "flex";
+          card.classList.remove("_hide");
+          card.classList.add("_show");
         } else {
-          card.style.display = "none";
+        
+          card.classList.add("_hide");
+          card.classList.remove("_show");
         }
+
+        
       });
     });
   }
